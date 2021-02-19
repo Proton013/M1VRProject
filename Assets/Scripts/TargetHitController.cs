f600a39b071911e9d3137be69560f18265a8d460
RemiArbache <32673658+RemiArbache@users.noreply.github.com>
2021-02-19 22:20:20 +0100

    Add outline on hit

--- Assets/Scripts/TargetHitController.cs
+++ Assets/Scripts/TargetHitController.cs
@@ -9,22 +9,26 @@ public class TargetHitController : MonoBehaviour
 
     private Renderer _targetRenderer;
     private Color _defaultColor;
-
+    private Outline _outline;
     private float _colorCount = 0f;
     private bool _hasTakenDamage = false;
 
     private void Start()
     {
         _targetRenderer = gameObject.transform.GetChild(0).GetComponent<Renderer>();
+        _outline = gameObject.transform.GetChild(0).GetComponent<Outline>();
+
         _defaultColor = _targetRenderer.material.color;
     }
 
     private void Update()
     {
-        if (_colorCount > 0.2f) // for 0.1s target's color is red
+        if (_colorCount > 0.2f) // for 0.2s target's color is red
         {
             _targetRenderer.material.color = _defaultColor;
             _hasTakenDamage = false;
+            _outline.OutlineWidth = 0f;
+
         }
         if (_hasTakenDamage) _colorCount += Time.deltaTime;
     }
@@ -35,6 +39,8 @@ public class TargetHitController : MonoBehaviour
         _hasTakenDamage = true;
         _colorCount = 0f;
         _targetRenderer.material.color = Color.red;
+        _outline.OutlineWidth = 10f;
+        
         
         lifespan--;
         if (lifespan <= 0)
